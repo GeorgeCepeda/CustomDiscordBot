71b30873fe5d03788442477c9ae1c3b25059e4a7
Geo Cepeda <Geoisking123@geos-imac.attlocal.net>
2020-01-20 17:05:02 -0600

    Added new SQL functions and function that adds text to an image.

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@
 import discord
 import sqlite3
 from sqlite3 import Error
+
+from PIL import Image, ImageDraw, ImageFont
 #import botfunctions as bf
 
 #Other Section
@@ -29,6 +31,24 @@ async def order(message):
     
     if cmd == '!latest':
         await newest_messages(message)
+
+    if cmd == '!show_log_for':
+        await person_log(message, msg[1])
+
+    if cmd == '!rate':
+        await rate_quote(message, msg[1], msg[2])
+
+    if cmd == '!top_msg':
+        await top_quotes(message)
+    
+    if cmd == '!memify':
+        await meme_this(message, msg[1])
+
+    if cmd == '!help':
+        helpmsg = "List of commands so far \n !intro - Introducs me \n !latest - gives last 3 recorded messages \n !show_log_for username - gives the last 3 messages from user specified \n !rate MessageId Rating - Will rate the message with the corresponding id the rating given \n !top_msg - shows the top 3 rated messages".format(message)
+        await message.channel.send(helpmsg)
+
+        
 
 #added in v2.1
 #sends a simple introduction message with a link to it's github repo
@@ -68,14 +88,111 @@ async def newest_messages(message):
             mId = row[1]
             writer = row[2]
             content = row[3]
-            rowMsg = "Message Id: " + str(mId) + ", Author: " + writer + ", Message: " + content + "\n"
+            rating = row[4]
+            rowMsg = "Message Id: " + str(mId) + ", Author: " + writer + ", Message: " + content + ", Rating: " + str(rating) + "\n"
             msg += rowMsg
         
         msg.format(message)
         await message.channel.send(msg)
     else:
         print("Could not connect for latest messages")
+
+#added in v2.1
+#Shows the last 3 messages of the person specified by the command.
+async def person_log(message, user):
+    #Message is the original message, it has the channel the original message came from
+    #User is the name of the user who's messages we want to check.
+    conn = create_connection(r"discordlog.db")
+    if conn is not None:
+        query = "SELECT * FROM messages WHERE Author = '" + user + "' ORDER BY id DESC LIMIT 3"
+        
+        curr = conn.cursor()
+        curr.execute(query)
+        rows = curr.fetchall()
+
+        msg = "Last 3 messages sent from this user:" + user + " Note there are only the latest.\n"
+        for row in rows:
+            mId = row[1]
+            writer = row[2]
+            content = row[3]
+            rating = row[4]
+            rowMsg = "Message Id: " + str(mId) + ", Author: " + writer + ", Message: " + content + ", Rating: " + str(rating) + "\n"
+            msg += rowMsg
+        
+        msg.format(message)
+        await message.channel.send(msg)
+    else:
+        print("Could not find user's latest message")
+
+#added in v2.1
+#updates rating of a message
+async def rate_quote(message, mId, rating):
+    #Message is the original message
+    #mId is the id of the message we want to update the rating for
+    #rating is the new rating 
+    conn = create_connection(r"discordlog.db")
+    if conn is not None:
+        update = "UPDATE messages SET Rating = " + rating + " WHERE MessageId = " + mId
+        
+        curr = conn.cursor()
+        curr.execute(update)
+        conn.commit()
+
+
+        msg = "The message rating for message " + mId + " has been updated!"
+        msg.format(message)
+        await message.channel.send(msg)
+    else:
+        print("Could not update message")
+
+#added in v2.1 
+#Gives a view of the top rated messages
+async def top_quotes(message):
+    conn = create_connection(r"discordlog.db")
+    if conn is not None:
+        query = "SELECT * FROM messages ORDER BY Rating DESC LIMIT 3"
+        
+        curr = conn.cursor()
+        curr.execute(query)
+        rows = curr.fetchall()
+
+        msg = "Top 3 rated messages from this server\n"
+        for row in rows:
+            mId = row[1]
+            writer = row[2]
+            content = row[3]
+            rating = row[4]
+            rowMsg = "Message Id: " + str(mId) + ", Author: " + writer + ", Message: " + content + ", Rating: " + str(rating) + "\n"
+            msg += rowMsg
+        
+        msg.format(message)
+        await message.channel.send(msg)
+    else:
+        print("Could not connect for highest rated messages")
     
+#added in v2.1
+#Checks if the message has an attachment, saves, edits and then sends it back
+async def meme_this(message, meme):
+    #gets info about the attachments
+    attach = message.attachments
+    if len(attach) == 0:
+        return #Can't do anything list is empty
+    else:
+        #saves the attachment as named
+        await attach[0].save("memeImage.jpeg")
+
+        #image editor
+        image = Image.open("memeImage.jpeg")
+        font_type = ImageFont.truetype('Impact.ttf',75)
+        draw = ImageDraw.Draw(image)
+        draw.text(xy=(50,50), text = meme, fill = (255,255,255), font = font_type)
+        #image.show() #shows our image, for testing purposes
+        image.save("memeImageEdit.PNG")
+
+        #send the image
+        await message.channel.send(file=discord.File('memeImageEdit.PNG'))
+
+
 #End of Custom Functions
 
 #SQLite functions
@@ -162,4 +279,3 @@ v1.0 - Original JavaScript version of bot. released: 2018
 v2.0 - Bot remade in python based from previous JavaScript version. released: December 2019
 V2.1 - Added SQLite database features. released: January 15, 2020
 """
-
